893a3c7a4f3ade5127143ebab9f326f189305d59
Candice Jovon Juarez <candice.artist@gmail.com>
2023-03-03 03:50:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const { off } = require("process")
+
+for (let i = 0; i <= 200; i++) {
+  // console.log(i,"Easy Going answered")
+
+}
+
+for (let i = 0; i <= 200; i++) {
+  // if(i % 2 === 0) {
+  //   console.log(i,"Get even answered")
+}
+
+
+for (let i = 0; i <= 100; i++) {
+  // if (i % 3 === 0 && i % 5 === 0) {
+  //   console.log(i, "Fizz Buzz")
+  // } else if (i % 3 === 0) {
+  //   console.log(i, "Fizz")
+  // } else if (i % 5 === 0) {
+  //   console.log(i, "Buzz")
+  // } else {
+  //   console.log("Fizz Buzz Answered")
+  // }
+}
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant", 5000, "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] = "5001"
+// console.log(plantee)
+wolfy[3] = "Gotham City"
+// console.log(wolfy)
+dart.push = "Hawkins"
+// console.log(dart)
+wolfy[0] = "Gameboy"
+// console.log(wolfy)
+
+const ninjaTurtles = ["Donatello, Leonardo, Raphael, Michaelangelo"]
+
+// for (string of ninjaTurtles){
+//   console.log(string.toUPperCase())
+// }
+// for (let i = 0; i < ninjaTurtles.length; i++) {
+    // console.log(ninjaTurtles[i])
+// }
+
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+
+// console.log(favMovies[8])
+
+// console.log(favMovies.sort())
+
+// console.log(favMovies.pop())
+
+// favMovies.push = "Guardians of the Galaxy"
+// console.log(favMovies)
+
+// console.log(favMovies.reverse())
+
+// console.log(favMovies.shift())
+
+// favMovies.unshift("Jaws")
+
+// favMovies.splice(favMovies.indexof("Django Unchained"), 1 "Avatar")
+
+// let size = favMovies.length
+// console.log(size)
+
+// let betterHalf = favMovies.slice(8, -1)
+// console.log(betterHalf)
+
+// console.log(favMovies.indexOf("Fast and the Furious 2"))
+
+const whereIsWaldo = [
+  ["Timmy", "Frank"],
+  "Eggbert",
+                    
+  ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    
+  ["Petunia", ["Baked Goods", "Waldo"]]
+];
+whereIsWaldo[Eggbert].remove()
